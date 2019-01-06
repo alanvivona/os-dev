8a550c30c9368a01938c1320e48f8126491e021c
Alan Vivona <alan.g.vivona@gmail.com>
2019-01-06 19:02:07 -0300

    added some color support and a draw line fn

--- dev/kernel.c
+++ dev/kernel.c
@@ -1,75 +1,155 @@
 #include <stddef.h>
 #include <stdint.h>
- 
+
 #if defined(__linux__)
-	#error "This code must be compiled with a cross-compiler"
+#error "This code must be compiled with a cross-compiler"
 #elif !defined(__i386__)
-	#error "This code must be compiled with an x86-elf compiler"
+#error "This code must be compiled with an x86-elf compiler"
 #endif
- 
-volatile uint16_t* vga_buffer = (uint16_t*)0xB8000;
+
+/* Defining boolean values */
+typedef enum
+{
+	false,
+	true
+} bool;
+
+/* VGA buffer definition START */
+volatile uint16_t *vga_buffer = (uint16_t *)0xB8000;
 const int VGA_COLS = 80;
 const int VGA_ROWS = 25;
- 
+
 int term_col = 0;
 int term_row = 0;
-uint8_t term_color = 0x0F;
- 
-void term_init()
+/* VGA buffer definition END */
+
+/* Color definitions START */
+uint8_t color_black = 0b00000000;
+uint8_t color_green = 0b00000001;
+uint8_t color_red = 0b00000010;
+uint8_t color_cyan = 0b00000011;
+uint8_t color_lgray = 0b00000111;
+uint8_t color_gray = 0b00001000;
+uint8_t color_white = 0b00001111;
+
+uint8_t term_color; // a single byte containing fg and bg color
+
+void term_set_colors(uint8_t *fg, uint8_t *bg)
 {
-	for (int col = 0; col < VGA_COLS; col ++)
+	term_color = *fg | (*bg << 4);
+}
+/* Color definitions END */
+
+uint8_t tab_size = 0b00000010;
+
+void term_clear()
+{
+	for (int row = 0; row < VGA_ROWS; row++)
 	{
-		for (int row = 0; row < VGA_ROWS; row ++)
+		for (int col = 0; col < VGA_COLS; col++)
 		{
 			const size_t index = (VGA_COLS * row) + col;
+			// Entries in the VGA buffer take the binary form BBBBFFFFCCCCCCCC, where:
+			// - B is the background color
+			// - F is the foreground color
+			// - C is the ASCII character
 			vga_buffer[index] = ((uint16_t)term_color << 8) | ' ';
 		}
 	}
 }
- 
-void term_putc(char c)
+
+void term_putc(char c, uint8_t *color)
 {
 	switch (c)
 	{
 	case '\n':
+		// support for new line
 		{
 			term_col = 0;
-			term_row ++;
+			term_row++;
 			break;
 		}
- 
-	default:
+	case '\t':
+		// support for tab
 		{
-			const size_t index = (VGA_COLS * term_row) + term_col;
-			vga_buffer[index] = ((uint16_t)term_color << 8) | c;
-			term_col ++;
+			term_col += tab_size;
 			break;
 		}
+	default:
+	{
+		const size_t index = (VGA_COLS * term_row) + term_col;
+		vga_buffer[index] = ((uint16_t)*color << 8) | c;
+		term_col++;
+		break;
 	}
- 
+	}
+
 	if (term_col >= VGA_COLS)
 	{
 		term_col = 0;
-		term_row ++;
+		term_row++;
 	}
- 
+
 	if (term_row >= VGA_ROWS)
 	{
 		term_col = 0;
 		term_row = 0;
 	}
 }
- 
-void term_print(const char* str)
+
+void term_print_string(const char *str, uint8_t *color)
 {
-	for (size_t i = 0; str[i] != '\0'; i ++)
-        term_putc(str[i]);
+	if (!color)
+	{
+		color = &term_color;
+	}
+	for (size_t i = 0; str[i] != '\0'; i++)
+		term_putc(str[i], color);
 }
- 
+
+void term_print_line(const char c, uint8_t *color, bool is_vertical)
+{
+	if (!color)
+	{
+		color = &term_color;
+	}
+
+	int limit = VGA_COLS;
+	if (is_vertical) {
+		int limit = VGA_ROWS;
+	}	
+
+	for (size_t i = 0; i < limit; i++)
+	{
+		term_putc(c, color);
+		
+		if (is_vertical) {
+			term_putc('\n', color);
+		}
+		
+	}
+}
 
 void kernel_main()
 {
-	term_init();
-    term_print("Hello, World!\n");
-	term_print("Welcome to the kernel.\n");
+	term_set_colors(&color_white, &color_black);
+	term_clear();
+
+	term_print_line('-', &term_color, 0);
+	term_print_line('-', &term_color, 1);
+
+	term_set_colors(&color_cyan, &color_black);
+	term_print_string("\tGreetings, human!\n", &term_color);
+
+	term_set_colors(&color_red, &color_black);
+	term_print_string("\tGreetings, human!\n", &term_color);
+
+	term_set_colors(&color_gray, &color_black);
+	term_print_string("\tGreetings, human!\n", &term_color);
+
+	term_set_colors(&color_lgray, &color_red);
+	term_print_string("\tGreetings, human!\n", &term_color);
+
+	term_set_colors(&color_green, &color_black);
+	term_print_string("\tGreetings, human!\n", &term_color);
 }
