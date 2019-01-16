58309af8845733f8801408bfee34dea1bdbe8d9b
Alan Vivona <alan.g.vivona@gmail.com>
2019-01-15 22:39:50 -0300

    now it can move chars around the vga buffer

--- dev/src/kernel.asm
+++ dev/src/kernel.asm
@@ -33,57 +33,80 @@ GLOBAL _Kernel_Start:function
 SECTION .text
 
 ; BEGIN - Multiboot Header
-MultibootSignature dd 464367618
-MultibootFlags dd 3
-MultibootChecksum dd -464367621
-MultibootGraphicsRuntime_VbeModeInfoAddr dd 2147483647
-MultibootGraphicsRuntime_VbeControlInfoAddr dd 2147483647
-MultibootGraphicsRuntime_VbeMode dd 2147483647
-MultibootInfo_Memory_High dd 0
-MultibootInfo_Memory_Low dd 0
+Multiboot.Signature dd 464367618
+Multiboot.Flags dd 3
+Multiboot.Checksum dd -464367621
+Multiboot.GraphicsRuntime_VbeModeInfoAddr dd 2147483647
+Multiboot.GraphicsRuntime_VbeControlInfoAddr dd 2147483647
+Multiboot.GraphicsRuntime_VbeMode dd 2147483647
+Multiboot.Info_Memory_High dd 0
+Multiboot.Info_Memory_Low dd 0
 ; END - Multiboot Header
 
-MultibootInfo_Structure dd 0
+Multiboot.Info_Structure dd 0
 
 ; VGA const definitions
-VGA_BUFFER equ 0xB8000
-VGA_COLS equ 80
-VGA_ROWS equ 25
-VGA_SIZE equ 2000
-VGA_BLOCK_SIZE equ 2
+VGA.Buffer equ 0x0B8000
+VGA.Cols equ 80
+VGA.Rows equ 25
+VGA.Lenght equ 2000
+VGA.BlockSize equ 2
+
+Text.Whitespace equ 0x0320 ; 0x0320 = 0 blakc bg, 3 cyan fg, 20 (" " == space)
 
 _Kernel_Start:
-	
-	mov eax, 0x0320 	; 0x0320 = 0 blakc bg, 3 cyan fg, 20 (" " == space)
-	call fillScreen
+	mov eax, Text.Whitespace 	
+	call _fillScreen
 	
 	mov eax, 0x1f41 	; 1 green bg, f white fg, 41 A
 	; pass address for the 3rd row, four spaces to the right (one tab) 
-	lea ebx, [ VGA_BUFFER + (VGA_COLS * 2 * VGA_BLOCK_SIZE) + (VGA_BLOCK_SIZE * 4) ]
-	call writeChar
+	lea ebx, [ VGA.Buffer ];+ (VGA.Cols * 2 * VGA.BlockSize) + (VGA.BlockSize * 4) ]
+	call _writeChar
+
+	lea ecx, [VGA.Lenght-1]
+	_Kernel_Start.loop:
+		push ecx
+		call _scroll
+		pop ecx
+		dec ecx
+	jnz _Kernel_Start.loop
 
 	hlt
 
-
-fillScreen:
+_fillScreen:
 	; ax = char to fill the screen with
-	push ebp
-	mov ebp, esp
-	mov ebx, VGA_BUFFER
-	mov ecx, VGA_SIZE
-	nextBufferSegment:
-	mov word [ebx], ax
-	add ebx, VGA_BLOCK_SIZE
-	loop nextBufferSegment
-	pop ebp
+	mov ebx, VGA.Buffer
+	mov ecx, VGA.Lenght
+	_fillScreen.loop:
+		mov word [ebx], ax
+		add ebx, VGA.BlockSize
+	loop _fillScreen.loop
 	ret
 
-writeChar:
+_writeChar:
 	; ax 	= char to fill the screen with
 	; ebx 	= address 
-	push ebp
-	mov ebp, esp
 	mov [ebx], ax
-	pop ebp
 	ret
+
+_scroll:
+	; start looping throught the buffer in reverse order to replace
+	; each byte with the content of the previous one
+	mov ecx, VGA.Lenght
+	_scroll.loop:
+		mov eax, [ VGA.Buffer + ((ecx-2)*VGA.BlockSize) ]
+		mov [ VGA.Buffer + ((ecx-1)*VGA.BlockSize) ], eax
+		dec ecx
+		cmp ecx, 1
+	jne _scroll.loop
+	
+	mov eax, Text.Whitespace
+	; pass address for the 1st square to fill it with an empty space
+	lea ebx, [ VGA.Buffer ]
+	call _writeChar
+	
+	ret
+		
+
+
 
